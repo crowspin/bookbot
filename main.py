d6c746e2b70485099a0b5023484f5d9ca1af7607
Heath Boutilier <heathboutilier@outlook.com>
2025-07-06 19:19:33 -0700

    'Lil extra spice, because I felt like it

--- main.py
+++ main.py
@@ -14,24 +14,24 @@ def main():
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     
-    file = sys.argv[1]
+    for file in sys.argv[1:]:
+        try:
+            txt = get_book_text(file)
+        except Exception as e:
+            print(e)
 
-    try:
-        txt = get_book_text(file)
-    except Exception as e:
-        print(e)
+        count = count_words(txt)
+        chars = count_characters(txt)
+        pairs = sort_dict_by_val(chars)
 
-    count = count_words(txt)
-    chars = count_characters(txt)
-    pairs = sort_dict_by_val(chars)
-
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {file}...")
-    print("----------- Word Count ----------")
-    print(f"Found {count} total words")
-    print("--------- Character Count -------")
-    for pair in pairs:
-        print(f"{pair['char']}: {pair['num']}")
-    print("============= END ===============")
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {file}...")
+        print("----------- Word Count ----------")
+        print(f"Found {count} total words")
+        print("--------- Character Count -------")
+        for pair in pairs:
+            print(f"{pair['char']}: {pair['num']}")
+        print("============= END ===============")
+        print("")
 
 main()
